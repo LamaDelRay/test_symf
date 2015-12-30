3f2acd9128d15c27316ef1fa38680678cffba6b9
tsalvetti_3octets <theo@Mac-mini-de-Chris.local>
2015-12-30 17:36:19 +0100

    added collection because not lazy

--- src/LamaDelRay/PlatformBundle/Form/AdvertType.php
+++ src/LamaDelRay/PlatformBundle/Form/AdvertType.php
@@ -19,8 +19,12 @@ class AdvertType extends AbstractType
         ->add('date', 'date')->add('title', 'text')
         ->add('content', 'textarea')->add('author','text')
         ->add('published', 'checkbox', array('required' => false))->add('image', new ImageType())
-        ->add('save', 'submit')
-        ;
+        ->add('categories', 'collection', array(
+            'type'      => new CategoryType(),
+            'allow_add' =>  true,
+            'allow_delete' => true
+        ))
+        ->add('save',   'submit');
     }
     
     /**
